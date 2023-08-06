dad78e6f7f69f79b875a6c373829838156824847
Krunal Hedaoo <37552920+djinn09@users.noreply.github.com>
2023-08-06 20:07:22 +0530

    Update class_based_gremalin.py

--- class_based_gremalin.py
+++ class_based_gremalin.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 from contextlib import contextmanager
 import time
 import queue
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Gauge, Histogram
 from gremlin_python.driver import client, serializer
 from decouple import config  # Import config from python-decouple
 
@@ -23,6 +23,15 @@ MIN_CONNECTIONS = config('MIN_CONNECTIONS', default=2, cast=int)
 MAX_CONNECTIONS = config('MAX_CONNECTIONS', default=10, cast=int)
 CONNECTION_TIMEOUT = config('CONNECTION_TIMEOUT', default=900, cast=int)
 MAX_CREATION_TIME = config('MAX_CREATION_TIME', default=5, cast=int)
+
+
+
+# Define Prometheus metrics
+connection_gauge = Gauge("connection_pool_connections", "Number of active connections")
+connection_creation_time_histogram = Histogram("connection_creation_time_seconds", "Histogram of connection creation times")
+query_execution_time_histogram = Histogram("query_execution_time_seconds", "Histogram of query execution times")
+query_execution_error_count = Gauge("query_execution_errors", "Number of query execution errors")
+
 
 # Gremlin Python connection class
 class GremlinConnection:
@@ -47,17 +56,32 @@ class GremlinConnection:
             self.failed_attempts += 1
             raise
 
+    # def execute_query(self, query: str):
+    #     try:
+    #         if not self.client:
+    #             raise Exception("Not connected to database")
+    #         result_set = self.client.submit(query)
+    #         results = result_set.all().result()
+    #         return results
+    #     except Exception as e:
+    #         logger.error("Query execution error: %s", str(e))
+    #         raise
+    
     def execute_query(self, query: str):
         try:
             if not self.client:
                 raise Exception("Not connected to database")
+            start_time = time.time()
             result_set = self.client.submit(query)
             results = result_set.all().result()
+            execution_time = time.time() - start_time
+            query_execution_time_histogram.observe(execution_time)
             return results
         except Exception as e:
             logger.error("Query execution error: %s", str(e))
+            query_execution_error_count.inc()
             raise
-
+    
     def close(self):
         try:
             if self.client:
@@ -73,7 +97,7 @@ class ConnectionPool:
         self.timeout = timeout
         self.max_creation_time = max_creation_time
         self.connections = queue.Queue()
-        self.connection_gauge = Gauge("connection_pool_connections", "Number of active connections")
+        self.connection_gauge = connection_gauge
         self.partitions = {}
 
         # Initialize the connection pool with minimum connections
@@ -89,7 +113,9 @@ class ConnectionPool:
             connection = GremlinConnection(context_id)
             connection.connect()
             creation_time = time.time() - start_time
-
+    
+            # Record connection creation time in histogram
+            connection_creation_time_histogram.observe(creation_time)
             # Check if connection creation time exceeds the limit
             if creation_time > self.max_creation_time:
                 logger.warning("Connection creation exceeded max time: %f seconds", creation_time)
@@ -203,19 +229,34 @@ connection_pool = ConnectionPool(
 class QueryInput(BaseModel):
     query: str
 
-# Enhanced route to execute a query using a connection from the pool with context
-@app.post("/execute_query")
-async def execute_query(
-    query_input: QueryInput, 
-    context: dict = Depends(connection_pool.get_request_context),
-    connection: GremlinConnection = Depends(connection_pool.get_connection)
-):
-    try:
-        result = connection.execute_query(query_input.query)
-        return {"result": result}
-    except Exception as e:
-        logger.error("Query execution error: %s", str(e))
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Query execution error")
+# # Enhanced route to execute a query using a connection from the pool with context
+# @app.post("/execute_query")
+# async def execute_query(
+#     query_input: QueryInput, 
+#     context: dict = Depends(connection_pool.get_request_context),
+#     connection: GremlinConnection = Depends(connection_pool.get_connection)
+# ):
+#     try:
+#         result = connection.execute_query(query_input.query)
+#         return {"result": result}
+#     except Exception as e:
+#         logger.error("Query execution error: %s", str(e))
+#         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Query execution error")
+
+
+# Route to execute a Gremlin query
+@app.post("/execute")
+async def execute_query(query: str, background_tasks: BackgroundTasks, request: Request):
+    context_id = request.headers.get("X-Context-ID")
+    if not context_id:
+        context_id = "default"
+    
+    with connection_pool.get_connection(context_id) as connection:
+        background_tasks.add_task(connection_pool.close_idle_connections)
+        results = connection.execute_query(query)
+        return results
+
+
 
 # Enhanced dynamic scaling route to adjust the connection pool size based on demand and context
 @app.post("/dynamic_scaling/{num_connections}")
