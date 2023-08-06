88d9039c4d06d237ea9db3eadca78d7fb43a00fd
Krunal Hedaoo <37552920+djinn09@users.noreply.github.com>
2023-08-06 17:40:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-#Import the websockets, queue, asyncio, and functools libraries
-import websockets
-import queue
+from fastapi import FastAPI, BackgroundTasks, Depends
 import asyncio
-import functools
+import websockets
+from gremlin_python.structure.graph import Graph
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+import queue
+import time
 
-# Define the Neptune endpoint and port
+app = FastAPI()
+
+# Neptune Configuration
 neptune_endpoint = 'your-neptune-endpoint'
 neptune_port = 8182
 
@@ -12,107 +16,85 @@ neptune_port = 8182
 min_connections = 5
 max_connections = 20
 
-# Define the connection time in seconds
-connection_time = 15 * 60
+# Connection timeout in seconds
+connection_timeout = 15 * 60
 
 # Create a queue object to store the connections
 connection_pool = queue.Queue(maxsize=max_connections)
 
+# Dictionary to track the usage status of connections
+connection_usage = {}
+
 # Get an event loop object
 loop = asyncio.get_event_loop()
 
-# Define a function to create a websocket connection with Neptune
-async def create_connection():
-    # Create a websocket object with the Neptune websocket URL and a timeout of 15 seconds
-    ws = await asyncio.wait_for(websockets.connect(f'wss://{neptune_endpoint}:{neptune_port}/gremlin'), timeout=15)
-    # Return the websocket object
-    return ws
-
-# Define a function to fill the connection pool with the minimum number of connections
-async def fill_connection_pool():
-    # Loop until the connection pool reaches the minimum size
-    while connection_pool.qsize() < min_connections:
-        # Create a new connection
-        ws = await create_connection()
-        # Put the connection in the queue
-        connection_pool.put(ws)
-        # Schedule a callback function to close and replace the connection after 15 minutes
-        loop.call_later(connection_time, close_and_replace_connection, ws)
-
-# Define a function to close and replace a connection in the pool
-def close_and_replace_connection(ws):
-    # Close the connection
-    loop.create_task(ws.close())
-    # Create a new connection
-    loop.create_task(replace_connection())
-
-# Define a function to create and put a new connection in the pool
-async def replace_connection():
-    # Create a new connection
-    ws = await create_connection()
-    # Try to put the connection in the queue
-    try:
-        # Put the connection in the queue without blocking
-        connection_pool.put(ws, block=False)
-        # Schedule a callback function to close and replace the connection after 15 minutes
-        loop.call_later(connection_time, close_and_replace_connection, ws)
-    except queue.Full:
-        # If the queue is full, close the connection
-        await ws.close()
-
-# Define a function to get a connection from the pool or create a new one if needed
+# Dependency to get a connection from the pool or create a new one if needed
 async def get_connection():
-    # Try to get a connection from the queue
     try:
-        # Get a connection from the queue without blocking
+        # Try to get a connection from the queue
         ws = connection_pool.get(block=False)
-        # Return the connection
+        connection_usage[ws] = True  # Mark the connection as in use
         return ws
     except queue.Empty:
         # If the queue is empty, create a new connection
         ws = await create_connection()
-        # Return the connection
+        connection_usage[ws] = True  # Mark the connection as in use
         return ws
 
-# Define a function to return a connection to the pool or close it if not needed
+# Dependency to return a connection to the pool or mark it as available
 async def return_connection(ws):
-    # Try to put the connection back in the queue
-    try:
-        # Put the connection in the queue without blocking
-        connection_pool.put(ws, block=False)
-    except queue.Full:
-        # If the queue is full, close the connection
-        await ws.close()
+    if ws in connection_usage:
+        connection_usage[ws] = False  # Mark the connection as available
+        try:
+            # Put the connection back in the queue without blocking
+            connection_pool.put(ws, block=False)
+        except queue.Full:
+            # If the queue is full, close the connection
+            await close_connection(ws)
 
-# Define a function to run a sample query using a connection from the pool
-async def run_query():
-    # Get a connection from the pool
+# Dependency to close a connection
+async def close_connection(ws):
+    if ws in connection_usage:
+        await ws.close()
+        del connection_usage[ws]
+
+# Define a function to create a websocket connection with Neptune
+async def create_connection():
+    ws = await asyncio.wait_for(websockets.connect(f'wss://{neptune_endpoint}:{neptune_port}/gremlin'), timeout=15)
+    connection_usage[ws] = True  # Mark the connection as in use
+    return ws
+
+# Define a function to execute a Gremlin query
+async def execute_query(query: str):
     ws = await get_connection()
-    # Create and return a remote connection object with the websocket
     remote_connection = DriverRemoteConnection(ws, 'g')
     try:
-        # Create a graph traversal object with the remote connection
         g = Graph().traversal().withRemote(remote_connection)
-        # Run a sample query to get the first two vertices from Neptune
-        result = g.V().limit(2).toList()
-        # Print the result
-        print(result)
+        result = g.V().hasLabel(query).toList()  # Modify the query as needed
+        return result
     finally:
-        # Return the connection to the pool
         await return_connection(ws)
 
-# Define an async main function to run multiple queries concurrently
-async def main():
+# Background task to periodically check and replace connections
+async def check_connection_timeouts():
+    while True:
+        await asyncio.sleep(60)  # Check every 1 minute
+        current_time = time.time()
+        for ws, in_use in connection_usage.items():
+            if not in_use and current_time - ws.creation_time > connection_timeout:
+                await close_connection(ws)
+                new_ws = await create_connection()
+                connection_pool.put(new_ws)
+                new_ws.creation_time = current_time
+
+@app.on_event("startup")
+async def startup_event():
     # Fill the connection pool with the minimum number of connections
     await fill_connection_pool()
-    # Create a list of tasks to run queries concurrently
-    tasks = [run_query() for _ in range(10)]
-    # Wait for all tasks to finish
-    await asyncio.gather(*tasks)
+    # Start the background task to check connection timeouts
+    loop.create_task(check_connection_timeouts())
 
-# Run the main function using an event loop
-loop.run_until_complete(main())
-
-# This code will create and maintain at least 5 websocket connections with Neptune, and use them to run queries concurrently. Each connection will have a connection time of 15 minutes, after which it will be closed and replaced by a new connection. If there are more requests than available connections, it will create new connections up to 20. If there are less requests than available connections, it will close some connections until there are only 5 left.
-
-# I hope this helps you understand how to use Amazon Neptune and FastAPI with connection pooling. If you have any other questions, please let me know. 😊
+@app.get('/')
+async def execute_gremlin_query(query: str):
+    result = await execute_query(query)
+    return {"result": result}
